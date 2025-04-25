9eb8ca8a6cc8bd6bfc9397922b94a1f1352c44f5
maudlyn01 <maudana2000@gmail.com>
2025-04-25 16:38:27 +0200

    feat:add function for btn manipulation

--- scripts/create-film.js
+++ scripts/create-film.js
@@ -4,16 +4,25 @@ export function newFilm(){
     
     return;
 }
+//Chilzia Macamo
  function addFilmForm(){
     return;
  }
-
+//Maud Uate
  function isFieldValidated(){
     return;
  }
+
+ //José Nono
  function addBtn(){
     return;
  }
+ //Afonso Relane
  function saveInDb(){
+    return;
+ }
+ //Moisés Ngovene
+ function btnManipulation(){
+
     return;
  }
