a9ea14610c0ff9526af55d0f4102ff008ff566d0
maudlyn01 <maudana2000@gmail.com>
2025-04-25 16:22:21 +0200

    feat:add functions to create-film

--- scripts/create-film.js
+++ scripts/create-film.js
@@ -1,1 +1,19 @@
+
 // Função para cadastrar novo filme
+export function newFilm(){
+    
+    return;
+}
+ function addFilmForm(){
+    return;
+ }
+
+ function isFieldValidated(){
+    return;
+ }
+ function addBtn(){
+    return;
+ }
+ function saveInDb(){
+    return;
+ }
