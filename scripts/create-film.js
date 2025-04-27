361b5c2fcae1e1cd98e980be5beece25e0f07d4e
maudlyn01 <maudana2000@gmail.com>
2025-04-27 18:07:14 +0200

    feat: create functionality to add new film, use html,css and js

--- scripts/create-film.js
+++ scripts/create-film.js
@@ -4,24 +4,40 @@ export function newFilm(){
     
     return;
 }
-//Chilzia Macamo
+function addBtn(){
+   const addNewFilm_popup = document.getElementById("add_newFilm_popup");
+   const addNewFilmBtn = document.getElementById("add_newFilm_btn");
+   const closePopupBtn = document.getElementById("close_popup_btn");
+
+   addNewFilmBtn.addEventListener("click",()=>{
+      addNewFilm_popup.style.display= "block";
+   });
+   closePopupBtn.addEventListener("click",()=>{
+      addNewFilm_popup.style.display="none";
+   });
+   window.addEventListener("click",(event)=>{
+      if(event.target===addNewFilm_popup){
+         addNewFilm_popup.style.display="none";
+      }
+   });
+
+   return;
+}
+
+
  function addFilmForm(){
     return;
  }
-//Maud Uate
+
  function isFieldValidated(){
     return;
  }
 
- //José Nono
- function addBtn(){
-    return;
- }
- //Afonso Relane
+ 
  function saveInDb(){
     return;
  }
- //Moisés Ngovene
+ 
  function btnManipulation(){
 
     return;
