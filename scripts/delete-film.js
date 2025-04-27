f7de819b820df7eb2b2443cef9c8fa5a79418008
Isa Neide Sitoe <issitoe941@gmail.com>
2025-04-27 21:37:23 +0200

    feat:create adelete button

--- scripts/delete-film.js
+++ scripts/delete-film.js
@@ -1,1 +1,10 @@
 // Função para eliminar filme
+import { films } from "../data/db.js";
+const deleteButton = document.createElement("button");
+deleteButton.textContent= "Delete";
+deleteButton.style.backgroundColor="red";
+deleteButton.style.border="1px solid black";
+/* deleteButton.addEventListener("click",()=>{
+    alert(`Tem certeza que deseja Eliminar ${films[1]}`);
+
+ });*/
