88b33aef3ea31c6b44697bf53f407fd776179c5f
neylachavane <neylachavane0@gmail.com>
2025-04-28 11:21:45 +0200

    feat: create a no button

--- scripts/delete-film.js
+++ scripts/delete-film.js
@@ -4,7 +4,16 @@ const deleteButton = document.createElement("button");
 deleteButton.textContent= "Delete";
 deleteButton.style.backgroundColor="red";
 deleteButton.style.border="1px solid black";
-/* deleteButton.addEventListener("click",()=>{
+/*deleteButton.addEventListener("click",()=>{
     alert(`Tem certeza que deseja Eliminar ${films[1]}`);
 
  });*/
+
+
+ //codigo Neyla
+const noButton = document.creatElement("button");
+noButton.textContent = "No";
+noButton.style.color = "red";
+noButton.addEventListner("click", function () {
+    noButton.style.display = "none";
+})
