2a6cfdcbed447ae4db91318010ee1aa8ddf10e38
esperancamunlela <esperancamunlela@gmail.com>
2025-04-28 15:13:41 +0200

    update: transform the form of update-film to a popup

--- scripts/update-film.js
+++ scripts/update-film.js
@@ -3,14 +3,23 @@ import { films } from "../data/db.js";
 //create form
 export function updateFilm() {
 const form = document.createElement("form");
+form.style.maxHeight = 'fit-content';
+form.style.maxWidth = 'fit-content'
+form.style.backgroundColor = '#D9D9D9';
+form.style.display = 'flex';
+form.style.flexDirection = 'column';
+form.style.padding = '20px';
+form.style.border = '1px solid black';
+form.style.margin = '10% auto'
+form.style.boxShadow = '4px 4px 5px gray';
 form.innerHTML=`
-
- <label for="title">Title</label>
+ <spam onclick="form.style.display='none'" id= "close_button">&times</spam>
+    <label for="title">Title</label>
  <input type="text" id="title" name="title" required><br>
 
  <label for="Gender">Gender</label>
- <input type="text" id= "gender" name="gender" required><br
- >
+ <input type="text" id= "gender" name="gender" required><br>
+ 
  <label for="year">year</label>
  <input type="number" id= "year" name="year" required><br>
 
@@ -24,7 +33,6 @@ form.innerHTML=`
  <input type="url"  id="imageUrl" name = "imageUrl" required><br>
  <button onclick="saveUpdateFilm()">Salvar</button>
  `;
-
  //validar os inputs, estlizar o formulario e guardar alteracoes no BD
 document.body.appendChild(form);
 preFillForm(films[0]);
@@ -38,6 +46,9 @@ function preFillForm(films) {
     document.getElementById('description').value=films.description;
     document.getElementById('imageUrl').value=films.imageUrl;
 }
+
+//create popup for form
+const edit_button = document.getElementById("edit_button");
 function saveUpdateFilm() {
     //funcao para transformar em string e voltar a mandar para bd;
     
