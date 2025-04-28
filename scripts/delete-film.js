f527b8afa381146eb022db7ec70644e783b24391
Isa Neide Sitoe <issitoe941@gmail.com>
2025-04-28 20:19:17 +0200

    fix: fix issues with a delete function

--- scripts/delete-film.js
+++ scripts/delete-film.js
@@ -1,10 +1,16 @@
 // Função para eliminar filme
 import { films } from "../data/db.js";
 const deleteButton = document.createElement("button");
+
 deleteButton.textContent= "Delete";
 deleteButton.style.backgroundColor="red";
 deleteButton.style.border="1px solid black";
-/* deleteButton.addEventListener("click",()=>{
-    alert(`Tem certeza que deseja Eliminar ${films[1]}`);
-
- });*/
+deleteButton.style.borderRadius="2px";
+ deleteButton.addEventListener("click",()=>{
+   for(let i=0;i<films.length;i++){
+    alert(`Tem certeza que deseja Eliminar ${films[i].title}`);}
+    function deleteFilm(id) {
+ 
+      
+    }
+ });
