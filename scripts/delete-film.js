f8f82c670bdb5ea3743c30632a3a0198617e6b38
gracabila <gracabilla002@gmail.com>
2025-04-28 12:05:29 +0200

    feat: create botton for acept  delete filme

--- scripts/delete-film.js
+++ scripts/delete-film.js
@@ -1,19 +1,30 @@
 // Função para eliminar filme
-import { films } from "../data/db.js";
+ /*import { films } from "../data/db.js";
 const deleteButton = document.createElement("button");
 deleteButton.textContent= "Delete";
 deleteButton.style.backgroundColor="red";
 deleteButton.style.border="1px solid black";
-/*deleteButton.addEventListener("click",()=>{
+deleteButton.addEventListener("click",()=>{
     alert(`Tem certeza que deseja Eliminar ${films[1]}`);
 
  });*/
 
 
  //codigo Neyla
-const noButton = document.creatElement("button");
+/*const noButton = document.creatElement("button");
 noButton.textContent = "No";
 noButton.style.color = "red";
 noButton.addEventListner("click", function () {
     noButton.style.display = "none";
+})*/
+
+
+//codigo graca 
+const yesButton = document.createElement("button");
+yesButton.textContent = "yes";
+yesButton.style.color = "green";
+yesButton.addEventListener("click",function(){
+    alert("Filme eliminado com sucesso");
+    yesButton.style.display = "none";
+    
 })
