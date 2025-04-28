746f7e7f25a1ea9b4a0c448eefbb5d6364f84285
MequeChiziane <mequechiziane@gmail.com>
2025-04-28 13:01:37 +0200

    feat: Modal de Detalhes

--- scripts/film-details.js
+++ scripts/film-details.js
@@ -1,1 +1,66 @@
 // Função para mostrar detalhes de um filme
+
+
+
+
+/*const mainDiv = document.createElement("div");
+
+mainDiv.style.height = "500px";
+mainDiv.style.width = "25%";
+
+const image = document.createElement("img");
+image.style.height = "300px";
+image.style.width = "100%";
+image.style.borderRadius = "10px";
+image.src =
+  "https://th.bing.com/th/id/R.9b1688aced5c649cc1bb899432b29aba?rik=GXeEOMydmO1gTw&pid=ImgRaw&r=0";
+
+mainDiv.appendChild(image);
+const title = document.createElement("h1");
+title.textContent = "O mal que voce plantou";
+
+const year = document.createElement("span");
+year.textContent = "2006";
+year.style.padding = "10px 10px";
+
+const gender = document.createElement("span");
+gender.textContent = "Romance";
+gender.style.padding = "10px 10px";
+gender.style.margin = "10px";
+
+const rate = document.createElement("span");
+rate.textContent = "7";
+
+const description = document.createElement("div");
+description.style.width = "97%";
+description.style.margin = "0px";
+
+description.textContent =
+  "aqui teremos a descricao do nosso filmes. Agora temos o teste final dessa div que nao sabiamos qual stress era.aqui teremos a descricao do nosso filmes. Agora temos o teste final dessa div que nao sabiamos qual stress era.aqui teremos a descricao do nosso filmes. Agora temos o teste final dessa div que nao sabiamos qual stress era.aqui teremos a descricao do nosso filmes. Agora temos o teste final dessa div que nao sabiamos qual stress era.aqui teremos a descricao do nosso filmes. Agora temos o teste final dessa div que nao sabiamos qual stress era.";
+description.style.padding = "5px 10px";
+
+const btnBack = document.createElement("button");
+btnBack.textContent = "X";
+btnBack.style.width = "30px";
+btnBack.style.borderRadius = "10px";
+btnBack.style.position = "fixed";
+btnBack.style.bottom = "925px";
+btnBack.style.left = "452px";
+
+mainDiv.appendChild(title);
+mainDiv.appendChild(year);
+mainDiv.appendChild(gender);
+mainDiv.appendChild(rate);
+mainDiv.appendChild(description);
+mainDiv.appendChild(btnBack);
+
+document.body.appendChild(mainDiv);
+
+// Evento abertura do modal
+
+// Evento fechamento do modal
+span.onclick = function () {
+  modal.style.display = "none";
+};*/
+
+
