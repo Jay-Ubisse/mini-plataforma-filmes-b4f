1f580900fd49a7e5d37f33443df5ea036f94b014
Mchilzia <127592658+Mchilzia@users.noreply.github.com>
2025-04-29 02:16:48 +0200

    modal stylization and the button

--- scripts/film-details.js
+++ scripts/film-details.js
@@ -1,4 +1,3 @@
-
 const detailsDiv = document.getElementById("content");
 
 
@@ -17,3 +16,34 @@ closeButton.onclick = function() {
   detailsDiv.style.display = "none";
 }
 
+
+
+// Estilização do modal
+detailsDiv.style.position = "fixed";          
+detailsDiv.style.top = "10px";
+detailsDiv.style.left = "500px";
+detailsDiv.style.width = "100vw";              
+detailsDiv.style.height = "85vh";             
+detailsDiv.style.backgroundColor = "#ffffff"; 
+detailsDiv.style.color = "#000000";          
+detailsDiv.style.justifyContent = "center";   
+detailsDiv.style.alignItems = "left";      
+detailsDiv.style.flexDirection = "column";    
+detailsDiv.style.textAlign = "left";        
+detailsDiv.style.padding = "40px";
+  
+    
+
+// Estilização do botão de fechar
+closeButton.style.position = "absolute";     
+closeButton.style.top = "5px";                 
+closeButton.style.right = "10px";            
+closeButton.style.backgroundColor= "red";      
+closeButton.style.color = "white";          
+closeButton.style.border = "none";              
+closeButton.style.padding = "10px 15px";    
+closeButton.style.cursor = "pointer";      
+closeButton.style.fontSize = "10px";            
+closeButton.style.borderRadius = "10px";    
+closeButton.style.zIndex = "1";  
+      
