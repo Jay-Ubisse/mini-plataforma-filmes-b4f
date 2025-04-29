8cf7f9648e06c2531c5debf4ae6849cd5ea2cb2b
isaneyde <issitoe941@gmail.com>
2025-04-29 17:04:19 +0200

    update:update a delete function

--- scripts/delete-film.js
+++ scripts/delete-film.js
@@ -1,12 +1,14 @@
 // Função para eliminar filme
 /* import { films } from "../data/db.js";*/
-const filmCard= document.getElementById("filmCard");
-filmCard.style.width="300px";
-filmCard.style.height="300px";
-filmCard.style.padding="50px";
-filmCard.style.border="1px solid black";
-filmCard.style.boxShadow="10px 10px 10px 10px gray";
-filmCard.style.alignSelf="center";
+const modal= document.getElementById("modal");
+modal.style.width="300px";
+modal.style.height="100px";
+modal.style.padding="20px";
+modal.style.border="1px solid black";
+modal.style.boxShadow="10px 10px 10px 10px gray";
+modal.style.display="none";
+modal.style.position="fixed";
+
 //obtencao e estilizacao do botao para deletar
 const deleteButton = document.getElementById("delete-btn");
 deleteButton.textContent= "Delete";
@@ -14,38 +16,30 @@ deleteButton.style.backgroundColor="red";
 deleteButton.style.color="white";
 deleteButton.style.border="1px solid black";
 deleteButton.style.borderRadius="10px";
-document.filmCard.appendChild(deleteButton);
+
 
 //evento que sera disparado ao clicar o botao de deletar
-deleteButton.addEventListener("click", deleteFilm);
- function deleteFilm(id) {
-    for (let i = 0; i < films.length; i++) {
-    alert(`Tem certeza que deseja eliminar ${films[i].title}`);
-    if (confirm("sim")){
-        alert("filme eliminado com sucesso")
-    }else{
-        window.close();
-    }
-   
-    
- }}
+deleteButton.addEventListener("click", ()=>{
+    modal.style.display="block";
+const content=document.getElementById("modal-content");
+
+});
  
 
- //criacao e estilizacao do botao sim
-const noButton = document.creatElement("button");
-noButton.textContent = "No";
-noButton.style.color = "red";
-document.deleteButton.appendChild(noButton);
-noButton.addEventListner("click", function () {
+
+ //estilizacao do botao nao
+const noButton = document.getElementById("nobutton");
+noButton.style.color="red";
+noButton.addEventListener("click", ()=> {
     noButton.style.display = "none";
-})
+});
 
 
-//criacao e estilizacao do botao nao
-const yesButton = document.createElement("button");
+//estilizacao do botao sim
+const yesButton = document.getElementById("yesbutton");
 yesButton.textContent = "yes";
 yesButton.style.color = "green";
-yesButton.addEventListener("click",function(){
+yesButton.addEventListener("click",()=>{
     alert("Filme eliminado com sucesso");
     yesButton.style.display = "none";
     
