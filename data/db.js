0db92d453a9c77ac47a20e619906d08e29f3adee
neylachavane <neylachavane0@gmail.com>
2025-04-30 15:19:53 +0200

    update: array

--- data/db.js
+++ data/db.js
@@ -20,7 +20,6 @@ export const films = [
   },
   {
     id: 3,
-<<<<<<< HEAD
     title: "Crepusculo",
     gender: "Romance",
     year: 2014,
@@ -32,34 +31,18 @@ export const films = [
   {
     id: 4,
     title: "The Beekeeper",
-    gender: "Accao",
+    gender: "Acção",
     year: 2024,
     rate: 5,
     description: "Filme de accao e suspense, de um homem aparentemente comum que esconde um segredo .",
     imageUrl: "https://image.tmdb.org/t/p/original/1vWI4E9Qf135X2cr2AJOZ0CcWS7.jpg",
-=======
-    title: "Créspulo",
-    gender: "Romance",
-    year: 2014,
-    rate: 5,
-    description: "Uma jornada através do mundo do teatro.",
-    imageUrl: "https://th.bing.com/th/id/R.805c1f0d204e1ebaad827bf5fa6a7084?rik=zUmbbk%2Bwr14tfw&pid=ImgRaw&r=0",
+
   },
-  {
-    id: 4,
-    title: "Casamento Sangrento",
-    gender: "Drama",
-    year: 2014,
-    rate: 5,
-    description: "A verdade por trás dos grooves .",
-    imageUrl: "https://image.tmdb.org/t/p/w1280/oKX8t52F25bkWUWVXiAUfTXFnLA.jpg",
->>>>>>> 0963c1223e1d45d86ccb2e6339dffaee1a4b9dd0
-  },
+  
   {
     id: 5, 
     title: "Red Notice",
-<<<<<<< HEAD
-    gender: "Accao",
+    gender: "Comédia, Acção",
     year: 2021,
     rate: 5,
     description: "Filme  que conta a historia de 2 criminosos maia procurados pela interpol.",
@@ -68,27 +51,12 @@ export const films = [
   {
     id: 6,
     title: "Casamento sangrento",
-    gender: " Accao",
+    gender: " Acção",
     year: 2025,
     rate: 5,
     description: "Assassinato em um casamento",
     imageUrl: "https://thaka.bing.com/th/id/OIP.mm1HRcdRuGDeAu_dip_A4gHaEK?w=332&h=187&c=7&r=0&o=5&pid=1.7",
-=======
-    gender: "Documentário",
-    year: 2014,
-    rate: 5,
-    description: "A mudança que decoreu na vida de um jovem, de programador para gestor.",
-    imageUrl: "https://blogs.uai.com.br/opipoqueiro/wp-content/uploads/sites/54/2021/11/Alerta-Vermelho-01.jpg",
-  },
-  {
-    id: 6,
-    title: "The Beekeeper",
-    gender: "Development",
-    year: 2025,
-    rate: 5,
-    description: "On the journey to kill Dev's who use chat GPT",
-    imageUrl: "https://image.tmdb.org/t/p/original/1vWI4E9Qf135X2cr2AJOZ0CcWS7.jpg",
->>>>>>> 0963c1223e1d45d86ccb2e6339dffaee1a4b9dd0
+
   },
 
 ];
