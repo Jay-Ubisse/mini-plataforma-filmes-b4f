d727fc4c702b9522cdb10c6c8918c15612061872
geniamaria <mariageniaabilio@gmail.com>
2025-04-30 09:55:09 +0200

    fixed: resolve conflict

--- scripts/main.js
+++ scripts/main.js
@@ -2,59 +2,38 @@
 import { updateFilm } from "./update-film.js";
 //import { listfilms } from "./get-films.js";
 //listfilms(films)
-import { newFilm} from "./create-film.js";
+import { newFilm } from "./create-film.js";
 /*
 
 ============== GROUP 1 LOGIC ==========
 
 */
-document.addEventListener(`DOMContentLoaded`,()=>{
-  newFilm();
+document.addEventListener(`DOMContentLoaded`, () => {
+    newFilm();
 
 });
 
-
-
 /*
-
 ============== GROUP 2 LOGIC ==========
-
 *//*
 edit_button.addEventListener("click",()=>{ 
     updateFilm();
 })*/
 window.updateFilm = updateFilm;
 
-
 /*
 ============== GROUP 3 LOGIC ==========
 */
-<<<<<<< HEAD
-
-
 
 /*
     ===========GROUP 3 LOGIC === === === =
-
-    
->>>>>>> 9f8282c0009f92b099a197137ea82f5f253dddf5*/
-=======
->>>>>>> a7756f7b781fd12064daf6b0c14241ed966a1d4c
-
 import { films } from "../data/db.js";
 import { listfilms } from "./get-films.js";
 listfilms(films)
-
-
-
-
 /*
 ============== GROUP 4 LOGIC ==========
 
 */
 
-<<<<<<< HEAD
 
 import { showDetails } from "./film-details";
-=======
->>>>>>> a7756f7b781fd12064daf6b0c14241ed966a1d4c
