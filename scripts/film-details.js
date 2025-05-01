8eebec7af6ce4f6c0382cea0439663b2afc9ba27
Jorge Langa <jorgebernardolanga@gmail.com>
2025-05-01 21:26:33 +0200

    Update:Add else case if film is not found

--- scripts/film-details.js
+++ scripts/film-details.js
@@ -2,28 +2,25 @@ import { films } from "../data/db.js";
 
 const detailsDiv = document.getElementById("content");
 
-
-
-
-
-
 let title = document.getElementById("title");
 let image = document.getElementById("image");
 let gender = document.getElementById("gender");
 let year = document.getElementById("year");
 let rate = document.getElementById("rate");
 let description = document.getElementById("description");
-
-
-
-
+const message=document.createElement("h3")
+message.textContent="Filme não encontrado!"
 export function getFilm(value) {
   const film = films.find((film) => film.id === Number(value));
 
-  if (!film)  return "Filme não encontrado!";
+  if (!film)  return detailsDiv.appendChild(message);
 
- return ` ${title.innerHTML=film.title}+${image.src=("src",film.imageUrl)} Genero: ${gender.innerHTML=film.gender}Ano de criação: ${year.innerHTML=film.year} Avaliação: ${rate.innerHTML=film.rate}+
- ${description.innerHTML=film.description}`
+ return ` ${title.innerHTML=film.title}
+          ${image.src=("src",film.imageUrl)} 
+          ${gender.innerHTML=film.gender} 
+          ${year.innerHTML=film.year} 
+          ${rate.innerHTML=film.rate}+
+          ${description.innerHTML=film.description}`
 
 
 
